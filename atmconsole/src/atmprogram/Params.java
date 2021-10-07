5809b194eaa0d5ac82777207171e1179df713b99
ODEZDEMONO-PC\User <odez.demono@gmail.com>
2021-10-07 22:00:55 +0700

    add params

---
+++ atmconsole/src/atmprogram/Params.java
@@ -0,0 +1,35 @@
+package atmprogram;
+
+public class Params {
+	private String parCmd;
+	private String parSrc;
+	private String parTgt;
+	private int parNom;
+	
+	public String getParCmd() {
+		return parCmd;
+	}
+	public void setParCmd(String parCmd) {
+		this.parCmd = parCmd;
+	}
+	public String getParSrc() {
+		return parSrc;
+	}
+	public void setParSrc(String parSrc) {
+		this.parSrc = parSrc;
+	}
+	public String getParTgt() {
+		return parTgt;
+	}
+	public void setParTgt(String parTgt) {
+		this.parTgt = parTgt;
+	}
+	public int getParNom() {
+		return parNom;
+	}
+	public void setParNom(int parNom) {
+		this.parNom = parNom;
+	}
+	
+	
+}
